2c6917ff299bd1280bb8b410fb4c4e21b3b63d60
Annette Kariuki <kariukian1@gmail.com>
2023-07-21 00:10:05 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "OLlie", "Aki"];
+
+function writeCards(cards) {
+    const thankYouMessages = [];
+    for (let i = 0; i < cards.length; i++) {
+      thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+      debugger;
+    }
+    return thankYouMessages;
+  }
+  writeCards (cards);
+
+  function countDown(number) {
+    let countdown=0;
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
